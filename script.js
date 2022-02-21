59f108491347dbdf328f698a15836e74cdb53561
danii10 <vargadani101@gmail.com>
2022-02-21 13:14:11 +0100

    nyolcadik

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function gomb(){
+function klikk(){
     var km=document.getElementById("km").value;
     var uzemar=document.getElementById("uzemar").value;
+    var utikoltseg=8*uzemar*km/100;
+    document.getElementById("elso").innerHTML=utikoltseg;
+    var amortizacio=km*15;
+    document.getElementById("masodik").innerHTML=amortizacio;
+    var osszkoltseg=amortizacio+utikoltseg;
+    document.getElementById("harmadik").innerHTML=osszkoltseg;
 }
